833dd575140eda87f79be892494599f22622d146
Nikolay Iliev <myquattrobg@gmail.com>
2017-01-24 15:58:07 +0200

    Adding buttons.

--- src/TotoGame.java
+++ src/TotoGame.java
@@ -4,6 +4,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JToggleButton;
+import java.awt.Font;
+import java.awt.Insets;
+
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TotoGame extends JFrame {
 
@@ -29,12 +42,37 @@ public class TotoGame extends JFrame {
 	 * Create the frame.
 	 */
 	public TotoGame() {
+		setTitle("Toto Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnNewButton = new JButton("Run game");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GenerateNumbersMatrix();
+				System.out.println("test");
+			}
+		});
+		btnNewButton.setBounds(58, 49, 89, 23);
+		contentPane.add(btnNewButton);
+
 	}
 
+	public void GenerateNumbersMatrix() {
+		setContentPane(contentPane);
+		//contentPane.setLayout(null);
+		JToggleButton toggleButton_01 = new JToggleButton("1");
+		toggleButton_01.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+			}
+		});
+		toggleButton_01.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		toggleButton_01.setBounds(108, 310, 30, 30);
+		toggleButton_01.setMargin(new Insets(0, 0, 0, 0));
+		contentPane.add(toggleButton_01);
+	}
 }
