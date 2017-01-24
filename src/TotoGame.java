981517ad8f09c5cd334b4f3b15acf2597e1b8aad
Nikolay Iliev <myquattrobg@gmail.com>
2017-01-24 14:28:18 +0200

    Start coding

--- src/TotoGame.java
+++ src/TotoGame.java
@@ -1,9 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class TotoGame {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+public class TotoGame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TotoGame frame = new TotoGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the frame.
+	 */
+	public TotoGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
