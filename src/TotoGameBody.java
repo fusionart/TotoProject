776516edbe415bff669eba9ca3416f4347be6b2b
Nikolay Iliev <myquattrobg@gmail.com>
2017-01-25 11:27:11 +0200

    Added visualization for drawn numbers

--- src/TotoGameBody.java
+++ src/TotoGameBody.java
@@ -29,9 +29,12 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 public class TotoGameBody extends JFrame {
 	public JButton btnRun;
+	public JToggleButton button;
 	public int userNumbersCounter = 0;
 	public int[] userNumbersArr = { 0, 0, 0, 0, 0, 0 };
 	public int[] totoNumbersArrFirstRound = new int[6];
@@ -46,6 +49,16 @@ public class TotoGameBody extends JFrame {
 	private JTextField userNumberFour;
 	private JTextField userNumberFive;
 	private JTextField userNumberSix;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextField drawNumberOne;
+	private JTextField drawNumberTwo;
+	private JTextField drawNumberThree;
+	private JTextField drawNumberFour;
+	private JTextField drawNumberFive;
+	private JTextField drawNumberSix;
+	private JRadioButton rdbtnDrawOne;
+	private JRadioButton rdbtnDrawTwo;
+	private JRadioButton rdbtnDrawThree;
 
 	/**
 	 * Launch the application.
@@ -69,72 +82,203 @@ public class TotoGameBody extends JFrame {
 	public TotoGameBody() {
 		setTitle("Toto Game");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 600);
+		setBounds(100, 100, 600, 750);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		btnRun = new JButton("Run");
+		btnRun = new JButton("Let's play TOTO");
+		btnRun.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnRun.setBackground(new Color(102, 153, 204));
 		btnRun.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				totoNumbers();
+				totoNumbersRoundOne();
+				totoNumbersRoundTwo();
+				totoNumbersRoundThree();
+				showDrawNumbers();
 			}
 		});
-		btnRun.setBounds(466, 527, 89, 23);
+		btnRun.setBounds(177, 400, 240, 30);
 		contentPane.add(btnRun);
-		
+		btnRun.setVisible(false);
+
 		lblPleaseSelect = new JLabel("Please select 6 numbers");
+		lblPleaseSelect.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblPleaseSelect.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPleaseSelect.setBounds(33, 22, 522, 53);
+		lblPleaseSelect.setBounds(200, 100, 200, 20);
 		contentPane.add(lblPleaseSelect);
-		
+
 		lblOurNumbers = new JLabel("Your numbers");
-		lblOurNumbers.setBounds(21, 86, 89, 14);
+		lblOurNumbers.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblOurNumbers.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOurNumbers.setBounds(220, 15, 160, 20);
 		contentPane.add(lblOurNumbers);
-		
+
 		userNumberOne = new JTextField();
+		userNumberOne.setForeground(new Color(204, 0, 0));
+		userNumberOne.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberOne.setBackground(new Color(255, 255, 204));
+		userNumberOne.setEditable(false);
 		userNumberOne.setHorizontalAlignment(SwingConstants.CENTER);
-		userNumberOne.setBounds(103, 73, 40, 40);
+		userNumberOne.setBounds(155, 50, 40, 40);
 		contentPane.add(userNumberOne);
 		userNumberOne.setColumns(10);
-		
+
 		userNumberTwo = new JTextField();
+		userNumberTwo.setForeground(new Color(204, 0, 0));
+		userNumberTwo.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberTwo.setBackground(new Color(255, 255, 204));
+		userNumberTwo.setEditable(false);
 		userNumberTwo.setHorizontalAlignment(SwingConstants.CENTER);
 		userNumberTwo.setColumns(10);
-		userNumberTwo.setBounds(153, 73, 40, 40);
+		userNumberTwo.setBounds(205, 50, 40, 40);
 		contentPane.add(userNumberTwo);
-		
+
 		userNumberThree = new JTextField();
+		userNumberThree.setForeground(new Color(204, 0, 0));
+		userNumberThree.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberThree.setBackground(new Color(255, 255, 204));
+		userNumberThree.setEditable(false);
 		userNumberThree.setHorizontalAlignment(SwingConstants.CENTER);
 		userNumberThree.setColumns(10);
-		userNumberThree.setBounds(203, 73, 40, 40);
+		userNumberThree.setBounds(255, 50, 40, 40);
 		contentPane.add(userNumberThree);
-		
+
 		userNumberFour = new JTextField();
+		userNumberFour.setForeground(new Color(204, 0, 0));
+		userNumberFour.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberFour.setBackground(new Color(255, 255, 204));
+		userNumberFour.setEditable(false);
 		userNumberFour.setHorizontalAlignment(SwingConstants.CENTER);
 		userNumberFour.setColumns(10);
-		userNumberFour.setBounds(253, 73, 40, 40);
+		userNumberFour.setBounds(305, 50, 40, 40);
 		contentPane.add(userNumberFour);
-		
+
 		userNumberFive = new JTextField();
+		userNumberFive.setForeground(new Color(204, 0, 0));
+		userNumberFive.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberFive.setBackground(new Color(255, 255, 204));
+		userNumberFive.setEditable(false);
 		userNumberFive.setHorizontalAlignment(SwingConstants.CENTER);
 		userNumberFive.setColumns(10);
-		userNumberFive.setBounds(303, 73, 40, 40);
+		userNumberFive.setBounds(355, 50, 40, 40);
 		contentPane.add(userNumberFive);
-		
+
 		userNumberSix = new JTextField();
+		userNumberSix.setForeground(new Color(204, 0, 0));
+		userNumberSix.setFont(new Font("Tahoma", Font.BOLD, 13));
+		userNumberSix.setBackground(new Color(255, 255, 204));
+		userNumberSix.setEditable(false);
 		userNumberSix.setHorizontalAlignment(SwingConstants.CENTER);
 		userNumberSix.setColumns(10);
-		userNumberSix.setBounds(353, 73, 40, 40);
+		userNumberSix.setBounds(405, 50, 40, 40);
 		contentPane.add(userNumberSix);
-		btnRun.setVisible(false);
+
+		rdbtnDrawOne = new JRadioButton("Draw One");
+		rdbtnDrawOne.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showDrawNumbers(totoNumbersArrFirstRound);
+			}
+		});
+		buttonGroup.add(rdbtnDrawOne);
+		rdbtnDrawOne.setBounds(86, 472, 109, 23);
+		contentPane.add(rdbtnDrawOne);
+		rdbtnDrawOne.setVisible(false);
+
+		rdbtnDrawTwo = new JRadioButton("Draw Two");
+		rdbtnDrawTwo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showDrawNumbers(totoNumbersArrSecondRound);
+			}
+		});
+		buttonGroup.add(rdbtnDrawTwo);
+		rdbtnDrawTwo.setBounds(236, 472, 109, 23);
+		contentPane.add(rdbtnDrawTwo);
+		rdbtnDrawTwo.setVisible(false);
+
+		rdbtnDrawThree = new JRadioButton("Draw Three");
+		rdbtnDrawThree.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showDrawNumbers(totoNumbersArrThirdRound);
+			}
+		});
+		buttonGroup.add(rdbtnDrawThree);
+		rdbtnDrawThree.setBounds(375, 472, 109, 23);
+		contentPane.add(rdbtnDrawThree);
+		rdbtnDrawThree.setVisible(false);
+
+		drawNumberOne = new JTextField();
+		drawNumberOne.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberOne.setForeground(new Color(0, 0, 0));
+		drawNumberOne.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberOne.setEditable(false);
+		drawNumberOne.setColumns(10);
+		drawNumberOne.setBackground(new Color(255, 204, 51));
+		drawNumberOne.setBounds(155, 523, 40, 40);
+		contentPane.add(drawNumberOne);
+		drawNumberOne.setVisible(false);
+
+		drawNumberTwo = new JTextField();
+		drawNumberTwo.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberTwo.setForeground(new Color(0, 0, 0));
+		drawNumberTwo.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberTwo.setEditable(false);
+		drawNumberTwo.setColumns(10);
+		drawNumberTwo.setBackground(new Color(255, 204, 51));
+		drawNumberTwo.setBounds(205, 523, 40, 40);
+		contentPane.add(drawNumberTwo);
+		drawNumberTwo.setVisible(false);
+
+		drawNumberThree = new JTextField();
+		drawNumberThree.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberThree.setForeground(new Color(0, 0, 0));
+		drawNumberThree.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberThree.setEditable(false);
+		drawNumberThree.setColumns(10);
+		drawNumberThree.setBackground(new Color(255, 204, 51));
+		drawNumberThree.setBounds(255, 523, 40, 40);
+		contentPane.add(drawNumberThree);
+		drawNumberThree.setVisible(false);
+
+		drawNumberFour = new JTextField();
+		drawNumberFour.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberFour.setForeground(new Color(0, 0, 0));
+		drawNumberFour.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberFour.setEditable(false);
+		drawNumberFour.setColumns(10);
+		drawNumberFour.setBackground(new Color(255, 204, 51));
+		drawNumberFour.setBounds(305, 523, 40, 40);
+		contentPane.add(drawNumberFour);
+		drawNumberFour.setVisible(false);
+
+		drawNumberFive = new JTextField();
+		drawNumberFive.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberFive.setForeground(new Color(0, 0, 0));
+		drawNumberFive.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberFive.setEditable(false);
+		drawNumberFive.setColumns(10);
+		drawNumberFive.setBackground(new Color(255, 204, 51));
+		drawNumberFive.setBounds(355, 523, 40, 40);
+		contentPane.add(drawNumberFive);
+		drawNumberFive.setVisible(false);
+
+		drawNumberSix = new JTextField();
+		drawNumberSix.setHorizontalAlignment(SwingConstants.CENTER);
+		drawNumberSix.setForeground(new Color(0, 0, 0));
+		drawNumberSix.setFont(new Font("Tahoma", Font.BOLD, 13));
+		drawNumberSix.setEditable(false);
+		drawNumberSix.setColumns(10);
+		drawNumberSix.setBackground(new Color(255, 204, 51));
+		drawNumberSix.setBounds(405, 523, 40, 40);
+		contentPane.add(drawNumberSix);
+		drawNumberSix.setVisible(false);
 
 		Action numberAction = new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//userNumbers.replaceSelection(e.getActionCommand());
-				//userNumbers.replaceSelection(", ");
+				// userNumbers.replaceSelection(e.getActionCommand());
+				// userNumbers.replaceSelection(", ");
 			}
 		};
 
@@ -147,7 +291,7 @@ public class TotoGameBody extends JFrame {
 			for (int j = 0; j < 7; j++) {
 				String text = String.valueOf(counter);
 				counter++;
-				JToggleButton button = new JToggleButton(text);
+				button = new JToggleButton(text);
 				button.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						JToggleButton tBtn = (JToggleButton) e.getSource();
@@ -166,13 +310,34 @@ public class TotoGameBody extends JFrame {
 				button.addActionListener(numberAction);
 				button.setFont(new Font("Tahoma", Font.PLAIN, 11));
 				button.setMargin(new Insets(0, 0, 0, 0));
-				button.setBounds(100 + offsetX, 250 + offsetY, 30, 30);
+				button.setBounds(177 + offsetX, 125 + offsetY, 30, 30);
 				offsetX += 35;
 				contentPane.add(button);
 				UIManager.put("ToggleButton.select", Color.ORANGE);
 				SwingUtilities.updateComponentTreeUI(button);
 			}
 		}
+	}
+
+	protected void showDrawNumbers() {
+		drawNumberOne.setVisible(true);
+		drawNumberTwo.setVisible(true);
+		drawNumberThree.setVisible(true);
+		drawNumberFour.setVisible(true);
+		drawNumberFive.setVisible(true);
+		drawNumberSix.setVisible(true);
+		rdbtnDrawOne.setVisible(true);
+		rdbtnDrawTwo.setVisible(true);
+		rdbtnDrawThree.setVisible(true);
+		btnRun.setVisible(false);
+	}
+	private void showDrawNumbers(int[] totoNumbers) {
+		drawNumberOne.setText(Integer.toString(totoNumbers[0]));
+		drawNumberTwo.setText(Integer.toString(totoNumbers[1]));
+		drawNumberThree.setText(Integer.toString(totoNumbers[2]));
+		drawNumberFour.setText(Integer.toString(totoNumbers[3]));
+		drawNumberFive.setText(Integer.toString(totoNumbers[4]));
+		drawNumberSix.setText(Integer.toString(totoNumbers[5]));
 	}
 
 	private void addNumberToUserNumbers(int number) {
@@ -184,7 +349,7 @@ public class TotoGameBody extends JFrame {
 			}
 		}
 		setRunButtonVisible(userNumbersCounter);
-		userNumbersArr=sortArrays(userNumbersArr);
+		userNumbersArr = sortArrays(userNumbersArr);
 		showUserNumbers(userNumbersArr);
 		System.out.println(" array - " + Arrays.toString(userNumbersArr));
 	}
@@ -197,43 +362,43 @@ public class TotoGameBody extends JFrame {
 			}
 		}
 		setRunButtonVisible(userNumbersCounter);
-		userNumbersArr=sortArrays(userNumbersArr);
+		userNumbersArr = sortArrays(userNumbersArr);
 		showUserNumbers(userNumbersArr);
 		System.out.println(" array - " + Arrays.toString(userNumbersArr));
 	}
-	
+
 	private void showUserNumbers(int[] userNumbersArr2) {
-		if (userNumbersArr2[0]!=0){
+		if (userNumbersArr2[0] != 0) {
 			userNumberOne.setText(Integer.toString(userNumbersArr2[0]));
 		} else {
 			userNumberOne.setText("");
 		}
-		if (userNumbersArr2[1]!=0){
+		if (userNumbersArr2[1] != 0) {
 			userNumberTwo.setText(Integer.toString(userNumbersArr2[1]));
 		} else {
 			userNumberTwo.setText("");
 		}
-		if (userNumbersArr2[2]!=0){
+		if (userNumbersArr2[2] != 0) {
 			userNumberThree.setText(Integer.toString(userNumbersArr2[2]));
 		} else {
 			userNumberThree.setText("");
 		}
-		if (userNumbersArr2[3]!=0){
+		if (userNumbersArr2[3] != 0) {
 			userNumberFour.setText(Integer.toString(userNumbersArr2[3]));
 		} else {
 			userNumberFour.setText("");
 		}
-		if (userNumbersArr2[4]!=0){
+		if (userNumbersArr2[4] != 0) {
 			userNumberFive.setText(Integer.toString(userNumbersArr2[4]));
 		} else {
 			userNumberFive.setText("");
 		}
-		if (userNumbersArr2[5]!=0){
+		if (userNumbersArr2[5] != 0) {
 			userNumberSix.setText(Integer.toString(userNumbersArr2[5]));
 		} else {
 			userNumberSix.setText("");
 		}
-		
+
 	}
 
 	private void setRunButtonVisible(int userNumbersCounter2) {
@@ -242,10 +407,10 @@ public class TotoGameBody extends JFrame {
 		} else {
 			btnRun.setVisible(false);
 		}
-
 	}
 
-	private void totoNumbers() {
+	// Toto numbers for three rounds, randomly generated
+	private void totoNumbersRoundOne() {
 		Random rnm = new Random();
 		Boolean checkRnmNum;
 		int rnmNum;
@@ -263,6 +428,46 @@ public class TotoGameBody extends JFrame {
 		}
 		totoNumbersArrFirstRound = sortArrays(totoNumbersArrFirstRound);
 		System.out.println(Arrays.toString(totoNumbersArrFirstRound));
+	}
+
+	private void totoNumbersRoundTwo() {
+		Random rnm = new Random();
+		Boolean checkRnmNum;
+		int rnmNum;
+		for (int i = 0; i < totoNumbersArrSecondRound.length; i++) {
+			do {
+				checkRnmNum = false;
+				rnmNum = rnm.nextInt(50);
+				for (int j = 0; j < i; j++) {
+					if (totoNumbersArrSecondRound[j] == rnmNum) {
+						checkRnmNum = true;
+					}
+				}
+			} while (rnmNum == 0 || checkRnmNum);
+			totoNumbersArrSecondRound[i] = rnmNum;
+		}
+		totoNumbersArrSecondRound = sortArrays(totoNumbersArrSecondRound);
+		System.out.println(Arrays.toString(totoNumbersArrSecondRound));
+	}
+
+	private void totoNumbersRoundThree() {
+		Random rnm = new Random();
+		Boolean checkRnmNum;
+		int rnmNum;
+		for (int i = 0; i < totoNumbersArrThirdRound.length; i++) {
+			do {
+				checkRnmNum = false;
+				rnmNum = rnm.nextInt(50);
+				for (int j = 0; j < i; j++) {
+					if (totoNumbersArrThirdRound[j] == rnmNum) {
+						checkRnmNum = true;
+					}
+				}
+			} while (rnmNum == 0 || checkRnmNum);
+			totoNumbersArrThirdRound[i] = rnmNum;
+		}
+		totoNumbersArrThirdRound = sortArrays(totoNumbersArrThirdRound);
+		System.out.println(Arrays.toString(totoNumbersArrThirdRound));
 	}
 
 	private int[] sortArrays(int[] arrayToSort) {
